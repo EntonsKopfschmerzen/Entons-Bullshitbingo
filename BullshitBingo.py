83d3b287f911616b505b35c137330977dd49d585
EntonsKopfschmerzen <scakmak2@hotmail.de>
2024-09-03 22:09:21 +0200

    Add files via upload
    pyqt5 -> pyqt6, word counter

--- BullshitBingo.py
+++ BullshitBingo.py
@@ -2,45 +2,50 @@ import sys
 import random
 import json
 import os
-from PyQt5.QtWidgets import (
+from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, 
-    QPushButton, QLabel, QGridLayout, QComboBox, QLineEdit, QMessageBox, QFileDialog, QShortcut, QSlider
+    QPushButton, QLabel, QGridLayout, QComboBox, QLineEdit, QMessageBox, QFileDialog, QSlider
 )
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QKeyEvent, QPixmap, QPainter, QKeySequence, QColor, QFont
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeyEvent, QPixmap, QPainter, QKeySequence, QColor, QFont, QShortcut
 
 
 class BingoCardWindow(QWidget):
-    def __init__(self, size, terms, opacity, parent=None):
+    def __init__(self, size, terms, opacity, parent=None, shuffle=True):
         super().__init__(parent)
         self.size = size
         self.terms = terms
+        self.shuffle = shuffle
         self.grid_layout = QGridLayout()
-        #self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.setWindowFlags(Qt.FramelessWindowHint)  # Entfernt die Titelleiste
-        self.setWindowOpacity(opacity) 
+
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint |
+                            Qt.WindowType.WindowStaysOnTopHint)  # Entfernt die Titelleiste
+        self.setWindowOpacity(opacity)
+    
         print("Opacity: ", opacity)
+        
 
-        cardLenght = ((self.size)*100)
-        cardHeight = cardLenght
-        self.setFixedSize(cardLenght, cardHeight)
-
+        cardLength = (self.size * 100)
+        cardHeight = cardLength
+        self.setFixedSize(cardLength, cardHeight)
 
         # Layout-Einstellungen
         self.grid_layout.setSpacing(0)  # Setze den Abstand zwischen den Widgets auf 0
         self.grid_layout.setContentsMargins(0, 0, 0, 0)  # Setze die Ränder des Layouts auf 0
 
         # Shuffle und Grid erstellen
-        random.shuffle(self.terms)
+        if self.shuffle == True:
+            random.shuffle(self.terms)
+
         self.buttons = []
         for i in range(self.size):
             row = []
             for j in range(self.size):
                 term = self.terms[i * self.size + j]
                 print(term)
-                feldText = self.textLaengeAnpassen(term) # type: ignore
+                feldText = self.textLaengeAnpassen(term)  # type: ignore
                 button = QPushButton(feldText)
-            
+                
                 button.setCheckable(True)
                 button.setFixedSize(100, 100)
                 button.setStyleSheet("""
@@ -66,16 +71,14 @@ class BingoCardWindow(QWidget):
                 row.append(button)
             self.buttons.append(row)
 
-        #Screenshot vom Fenster machen
-        self.shortcut_export = QShortcut(QKeySequence("Ctrl+Q"), self)
+        # Shortcuts erstellen
+        self.shortcut_export = QShortcut(QKeySequence('Ctrl+Q'), self)
         self.shortcut_export.activated.connect(lambda: self.safeCardAsScreenshot(True))
 
-        #Karte als json Datei exportieren zum Teilen mit anderen
-        self.shortcut_export = QShortcut(QKeySequence("Ctrl+E"), self)
+        self.shortcut_export = QShortcut(QKeySequence('Ctrl+E'), self)
         self.shortcut_export.activated.connect(lambda: self.export_card(False))
 
-        #Karte als json Datei exportieren zum Speichern und später weitermachen
-        self.shortcut_export = QShortcut(QKeySequence("Ctrl+S"), self)
+        self.shortcut_export = QShortcut(QKeySequence('Ctrl+S'), self)
         self.shortcut_export.activated.connect(lambda: self.export_card(True))
 
         self.setLayout(self.grid_layout)
@@ -83,10 +86,6 @@ class BingoCardWindow(QWidget):
         # Fenster ziehen ermöglichen
         self.old_pos = None
 
-
-
-
-    # Ersetze den bisherigen Inhalt der format_text-Methode durch den folgenden Code:
     def textLaengeAnpassen(self, text):
         max_length = 9
         words = text.split(' ')
@@ -117,43 +116,44 @@ class BingoCardWindow(QWidget):
 
         return '\n'.join(lines)
 
-
     def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
-            self.old_pos = event.globalPos()
+        if event.button() == Qt.MouseButton.RightButton:
+            self.old_pos = event.globalPosition()
     
     def mouseMoveEvent(self, event):
         if self.old_pos:
-            delta = event.globalPos() - self.old_pos
-            self.move(self.x() + delta.x(), self.y() + delta.y())
-            self.old_pos = event.globalPos()
+            delta = event.globalPosition() - self.old_pos
+            self.move(self.x() + int(delta.x()), int(self.y()) + int(delta.y()))
+            self.old_pos = event.globalPosition()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.RightButton:
+        if event.button() == Qt.MouseButton.RightButton:
             self.old_pos = None
 
     def export_card(self, marked):
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Karte exportieren", "", "JSON Files (*.json)", options=options)
-        size = self.size
-        terms = self.terms
-        if marked == True:
-            marked_status = [[self.buttons[i][j].isChecked() for j in range(size)] for i in range(size)]
-        else:
-            marked_status = [[False for j in range(size)] for i in range(size)]
-        
-        data = {'size': size, 'terms': terms, 'marked': marked_status}
-        with open(filename, 'w') as f:
-            json.dump(data, f)
+        #options = QFileDialog.Option()
+        filename, _ = QFileDialog.getSaveFileName(self, "Karte exportieren", "", "JSON Files (*.json)")
+        if filename:
+            size = self.size
+            terms = self.terms
+            if marked:
+                marked_status = [[self.buttons[i][j].isChecked() for j in range(size)] for i in range(size)]
+            else:
+                marked_status = [[False for j in range(size)] for i in range(size)]
+            
+            data = {'size': size, 'terms': terms, 'marked': marked_status}
+            with open(filename, 'w') as f:
+                json.dump(data, f)
 
     def safeCardAsScreenshot(self, marked=False):
-            # Screenshot des aktuellen Bingo-Karten-Fensters
+        # Screenshot des aktuellen Bingo-Karten-Fensters
         pixmap = self.grab()
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Karte exportieren", "", "PNG Files (*.png)", options=options)
-        # Speichern des Screenshots als Bilddatei
-        pixmap.save(filename)
-        print(f"Karte erfolgreich als {filename} gespeichert.")
+        filename, _ = QFileDialog.getSaveFileName(self, "Karte exportieren", "", "PNG Files (*.png)")
+        if filename:
+            # Speichern des Screenshots als Bilddatei
+            pixmap.save(filename)
+            print(f"Karte erfolgreich als {filename} gespeichert.")
+
 
 
 
@@ -172,9 +172,11 @@ class BingoApp(QMainWindow):
         self.size_label = QLabel("Feldgröße:")
         self.size_combo = QComboBox()
         self.size_combo.addItems(["3x3", "4x4", "5x5"])
+        self.word_count_label = QLabel("Wörter eingegeben: 0")
 
         self.word_input = QLineEdit()
-        self.word_input.setPlaceholderText("Gib die Wörter mit Kommas getrennt ein")
+        self.word_input.setPlaceholderText("Gib die Wörter mit Komma getrennt ein: Apfel, Keks, Baum...")
+        self.word_input.textChanged.connect(self.update_word_count)
 
         self.create_button = QPushButton("Karte erstellen")
         self.create_button.clicked.connect(self.create_card)
@@ -183,7 +185,7 @@ class BingoApp(QMainWindow):
         self.import_button.clicked.connect(self.import_card)
 
         self.opacitySlider = QSlider()
-        self.opacitySlider.setOrientation(Qt.Horizontal)
+        self.opacitySlider.setOrientation(Qt.Orientation.Horizontal)
         self.opacitySlider.setMinimum(0)
         self.opacitySlider.setMaximum(80) 
         self.opacitySlider.setValue(0)
@@ -194,6 +196,7 @@ class BingoApp(QMainWindow):
 
         self.layout.addWidget(self.size_label)
         self.layout.addWidget(self.size_combo)
+        self.layout.addWidget(self.word_count_label)
         self.layout.addWidget(self.word_input)
         self.layout.addWidget(self.create_button)
         self.layout.addWidget(self.import_button)        
@@ -204,6 +207,12 @@ class BingoApp(QMainWindow):
         size = self.opacitySlider.value()
         self.opacityLabel.setText(f"Transparenz: {size}%")
 
+    def update_word_count(self):
+        text = self.word_input.text()
+        words = text.split(', ')
+        num_words = len(words)
+        self.word_count_label.setText(f"Wörter eingegeben: {num_words}")
+
     def create_card(self):
         
         size_str = self.size_combo.currentText()
@@ -213,24 +222,24 @@ class BingoApp(QMainWindow):
         if len(terms) < size * size:
             QMessageBox.warning(self, "Fehler", f"Du brauchst mindestens {size * size} Wörter!")
             return
-
         self.opacity_level = (100-self.opacitySlider.value())/100
-        self.card_window = BingoCardWindow(size, terms, self.opacity_level)
+        self.card_window = BingoCardWindow(size, terms, self.opacity_level, shuffle=True)
+        self.card_window.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, False)
         self.card_window.show()
 
         layout = QVBoxLayout()
         self.card_window.setLayout(layout)
 
     def import_card(self):
-        options = QFileDialog.Options()
-        filename, _ = QFileDialog.getOpenFileName(self, "Karte importieren", "", "JSON Files (*.json)", options=options)
+        filename, _ = QFileDialog.getOpenFileName(self, "Karte importieren", "", "JSON Files (*.json)")
         if filename:
             with open(filename, 'r') as f:
                 data = json.load(f)
                 size = data['size']
                 terms = data['terms']
                 self.opacity_level = (100-self.opacitySlider.value())/100
-                self.card_window = BingoCardWindow(size, terms, self.opacity_level)
+                self.card_window = BingoCardWindow(size, terms, self.opacity_level, shuffle=False)
+                self.card_window.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, False)    
                 self.card_window.show()
 
                 for i in range(size):
@@ -252,4 +261,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = BingoApp()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
